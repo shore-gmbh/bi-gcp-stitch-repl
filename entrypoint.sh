bfe728e37740890d4b1bdd51ec9157d45969b9ec
Francisco DaCunha <36667177+francisdcunha07@users.noreply.github.com>
2024-04-24 13:30:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,7 @@ airflow db reset
 
 #supervisord -c /path/to/supervisord.conf
 
+airflow users create -u franc_23 -f francis -l dcunha -r Admin -e francis.dcunha@shore.com -p 321aydc11be
+
 # Run the supervisor to manage the Airflow processes
 exec supervisord -c /etc/supervisord.conf
